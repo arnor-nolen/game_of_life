740c65be7325bb3f390c1c2f45638a91119cbad4
host-ru <andrey1gorbulin@gmail.com>
2018-10-10 03:38:38 +0300

    First version
    Initial implemetation of Game of Life. 18x10 resolution, unchangable.

---
+++ Game of Life/Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GameManager : MonoBehaviour {
+
+    public Tile deadTile;
+    public Tile aliveTile;
+    public Tilemap tilemap;
+    Generation generation = new Generation();
+    
+    public int tilemapWidth = 18;
+    public int tilemapHeight = 10;
+    public int seed;
+
+	// Use this for initialization
+	void Start () {
+        tilemap.size = new Vector3Int(tilemapWidth, tilemapHeight, 0);
+        tilemap.ResizeBounds();
+        Generation.CellState [,] cells = generation.Init(seed, tilemapWidth, tilemapHeight);
+        DrawGeneration(cells);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        DrawGeneration(generation.Next());
+	}
+
+    void DrawGeneration(Generation.CellState[,] cells)
+    {
+        for (int i = 0; i < cells.GetLength(0); i++)
+            for (int j = 0; j < cells.GetLength(1); j++)
+            {
+                Tile tileToDraw;
+                if (cells[i, j] == Generation.CellState.Dead)
+                    tileToDraw = deadTile;
+                else if (cells[i, j] == Generation.CellState.Alive)
+                    tileToDraw = aliveTile;
+                else
+                    tileToDraw = null;
+                tilemap.SetTile(new Vector3Int(i - cells.GetLength(0)/2, j - cells.GetLength(1)/2, 0), tileToDraw);
+            }
+    }
+}
---
+++ Game of Life/Assets/Scripts/Generation.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Generation {
+
+    private CellState[,] cells;
+    private int generationNumber;
+
+    public enum CellState
+    {
+        Dead = 0,
+        Alive = 1
+    }
+
+    public CellState[,] Next()
+    {
+        for (int i = 0; i < cells.GetLength(0); i++)
+            for (int j = 0; j < cells.GetLength(1); j++)
+            {
+                List<CellState> neighbors = GetNeighbors(i, j);
+                int aliveNeighbors = 0;
+                foreach (var neighbor in neighbors)
+                    if (neighbor == CellState.Alive)
+                        aliveNeighbors++;
+                if (cells[i, j] == CellState.Alive)
+                {
+                    if (aliveNeighbors == 2 || aliveNeighbors == 3)
+                        cells[i, j] = CellState.Alive;
+                    else
+                        cells[i, j] = CellState.Dead;
+                }
+                else
+                {
+                    if (aliveNeighbors == 3)
+                        cells[i, j] = CellState.Alive;
+                }
+            }
+        generationNumber++;
+        return cells;
+    }
+
+    public CellState[,] Init(int seed, int tilemapWidth, int tilemapHeight)
+    {
+        cells = new CellState[tilemapWidth, tilemapHeight];
+        Random.InitState(seed);
+        for (int i = 0; i < tilemapWidth; i++)
+            for (int j = 0; j < tilemapHeight; j++)
+            {
+                cells[i, j] = (CellState)Random.Range(0, 2);
+            }
+        generationNumber = 0;
+        return cells;
+    }
+
+    private List<CellState> GetNeighbors(int x, int y)
+    {
+        int cellsX = cells.GetLength(0) - 1;
+        int cellsY = cells.GetLength(1) - 1;
+        List<CellState> neighbors = new List<CellState>();
+        if (x > 0 && y > 0)
+            neighbors.Add(cells[x - 1, y - 1]);
+        if (x > 0)
+            neighbors.Add(cells[x - 1, y]);
+        if (y > 0)
+            neighbors.Add(cells[x, y - 1]);
+        if (x > 0 && y < cellsY)
+            neighbors.Add(cells[x - 1, y + 1]);
+        if (x < cellsX && y > 0)
+            neighbors.Add(cells[x + 1, y - 1]);
+        if (x < cellsX && y < cellsY)
+            neighbors.Add(cells[x + 1, y + 1]);
+        if (x < cellsX)
+            neighbors.Add(cells[x + 1, y]);
+        if (y < cellsY)
+            neighbors.Add(cells[x, y + 1]);
+        return neighbors;
+    }
+}
